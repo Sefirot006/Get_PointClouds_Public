cd10fa48c5bb2a90ed6543ba12ab2c9d3d6f1be4
SergioEspi <sergioespijimenez@gmail.com>
2016-05-08 11:54:22 +0200

    Añado para sacar dos nubes de puntos y el metodo pfhh para la extraccion de features, ahora voy a por la union

--- src/node.cpp
+++ src/node.cpp
@@ -18,11 +18,18 @@
 #include <pcl/common/io.h>
 #include <pcl/keypoints/sift_keypoint.h>
 #include <pcl/features/normal_3d.h>
+#include <pcl/features/pfh.h>
+#include <pcl/features/fpfh.h>
+#include <pcl/features/fpfh_omp.h>
+#include <pcl/features/pfhrgb.h>
 
-pcl::PointCloud<pcl::PointXYZRGB>::Ptr visu_pc (new pcl::PointCloud<pcl::PointXYZRGB>);
-pcl::PointCloud<pcl::PointXYZRGB>::Ptr visu_pc2 (new pcl::PointCloud<pcl::PointXYZRGB>);
+
+pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_1 (new pcl::PointCloud<pcl::PointXYZRGB>);
+pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_2 (new pcl::PointCloud<pcl::PointXYZRGB>);
 
 using namespace pcl;
+
+bool empieza = true;
 
 bool driveKeyboard(ros::Publisher cmd_vel_pub_)
 {
@@ -73,9 +80,11 @@ bool driveKeyboard(ros::Publisher cmd_vel_pub_)
   return true;
 }
 
-void detect_keypoints(pcl::PointCloud<pcl::PointXYZRGB>::Ptr &points,float min_scale,int nr_octaves,int nr_scales_per_octave,float min_contrast)
+void detect_keypoints(pcl::PointCloud<pcl::PointXYZRGB>::Ptr &points, PointCloud<PointWithScale> &keypoints, float min_scale,int nr_octaves,int nr_scales_per_octave,float min_contrast)
 {
-  PointCloud<pcl::PointWithScale> result;
+  //cout << "entro en la deteccion de los keypoints" << endl;
+  PointCloud<pcl::PointWithScale>::Ptr result (new PointCloud<PointWithScale>);
+  PointCloud<pcl::PointWithScale>::Ptr prueba (new PointCloud<PointWithScale>);
   SIFTKeypoint<PointXYZRGB, PointWithScale> sift_detect;
   // Use a FLANN-based KdTree to perform neighborhood searches
   search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ> ());
@@ -85,39 +94,137 @@ void detect_keypoints(pcl::PointCloud<pcl::PointXYZRGB>::Ptr &points,float min_s
   // Set the input
   sift_detect.setInputCloud(points);
   // Detect the keypoints and store them in "keypoints_out"
-  sift_detect.compute(result);
-  std::cout << "No of SIFT points in the result are " << result.points.size () << std::endl;
+  //cout << "a punto de compute" << endl;
+  sift_detect.compute(*result);
+  //cout << "PAso el coumpute" << endl;
+  prueba = result;
+
+  PointCloud<pcl::PointWithScale>::Ptr keypoints_ptr (new PointCloud<PointWithScale>);
+  copyPointCloud(*prueba, *keypoints_ptr);
+
+  //std::cout << "No of SIFT points in the result are " << keypoints_ptr->points.size () << std::endl;
+
+  copyPointCloud(*keypoints_ptr,keypoints);
 }
 
-void simpleVis ()
-{
+void simpleVis (){
   	pcl::visualization::CloudViewer viewer ("Simple Cloud Viewer");
-	while(!viewer.wasStopped())
-	{
-	  viewer.showCloud (visu_pc);
+	while(!viewer.wasStopped()){
+	  viewer.showCloud (cloud_2);
 	  boost::this_thread::sleep(boost::posix_time::milliseconds(1000));
 	}
 
 }
 
+void PFH(PointCloud<PointXYZRGB>::Ptr &points, PointCloud<Normal>::Ptr &normals, PointCloud<PointWithScale>::Ptr &keypoints, 
+          PointCloud<PFHSignature125> &descriptors_out, float feature_radius){
+  cout << "entro en el pfh" << endl;
+  cout << "Dentro del pfh, keypoints: " << keypoints->points.size() << endl;
+
+  PointCloud<PFHSignature125>::Ptr result(new PointCloud<PFHSignature125>);
+  PointCloud<PFHSignature125>::Ptr prueba(new PointCloud<PFHSignature125>);
+  
+  // Creamos un objreoto PFHEstimation
+  PFHEstimation<PointXYZRGB, Normal, PFHSignature125> pfh_est;
+  pfh_est.setSearchMethod(search::KdTree<PointXYZRGB>::Ptr (new search::KdTree<PointXYZRGB>));
+
+  //Especifica el radio de la caracteristica pfh
+  pfh_est.setRadiusSearch(feature_radius);
+
+  PointCloud<pcl::PointXYZRGB>::Ptr keypoints_xyzrgb (new PointCloud<PointXYZRGB>);
+  copyPointCloud(*keypoints, *keypoints_xyzrgb);
+
+  // Usar todos los puntos para aalizar la estructura local de la nube
+  pfh_est.setSearchSurface(points);
+  pfh_est.setInputNormals(normals);
+
+  // Pero solo computa las caracteristicas de los keypoints
+  pfh_est.setInputCloud(keypoints_xyzrgb);
+
+
+  cout << "a punto de computar" << endl;
+  // Computa las caracteristicas
+  pfh_est.compute(*result);
+
+  prueba = result;
+
+  PointCloud<PFHSignature125>::Ptr result_ptr(new PointCloud<PFHSignature125>);
+  copyPointCloud(*prueba,*result_ptr);
+
+  //std::cout << "No of PFH points in the descriptors are " << result_ptr->points.size () << std::endl;
+
+  copyPointCloud(*result_ptr,descriptors_out);
+
+}
+
+void compute_surface_normals(PointCloud<PointXYZRGB>::Ptr &points, float normal_radius, PointCloud<Normal>::Ptr &normals_out){
+  NormalEstimation<PointXYZRGB,Normal> norm_est;
+  norm_est.setSearchMethod(search::KdTree<PointXYZRGB>::Ptr (new search::KdTree<PointXYZRGB>));
+  norm_est.setRadiusSearch(normal_radius);
+  norm_est.setInputCloud(points);
+  norm_est.compute(*normals_out);
+}
+
 void callback(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr& msg)
 {
-	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>(*msg));
-	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZRGB>);
+  if(empieza==true){
+    cout << "entro por el if" << endl;
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>(*msg));  
+    copyPointCloud(*cloud, *cloud_1);
+    cout << "Puntos capturados_1: " << cloud->size() << endl;
+    empieza = false;
+  }
+  else{
+    cout << "entro por el else" << endl;
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_aux (new pcl::PointCloud<pcl::PointXYZRGB>(*msg));  
+    copyPointCloud(*cloud_aux, *cloud_2);
+    cout << "Puntos capturados_2: " << cloud_aux->size() << endl;
 
-	cout << "Puntos capturados: " << cloud->size() << endl;
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered_1 (new pcl::PointCloud<pcl::PointXYZRGB>);
+    PointCloud<pcl::PointWithScale>::Ptr pcKeyPoints_1 (new pcl::PointCloud<pcl::PointWithScale>);
+    PointCloud<PFHSignature125>::Ptr cloudDescriptors_1 (new pcl::PointCloud<PFHSignature125>);
 
-	pcl::VoxelGrid<pcl::PointXYZRGB > vGrid;
-	vGrid.setInputCloud (cloud);
-	vGrid.setLeafSize (0.05f, 0.05f, 0.05f);
-	vGrid.filter (*cloud_filtered);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered_2 (new pcl::PointCloud<pcl::PointXYZRGB>);
+    PointCloud<pcl::PointWithScale>::Ptr pcKeyPoints_2 (new pcl::PointCloud<pcl::PointWithScale>);
+    PointCloud<PFHSignature125>::Ptr cloudDescriptors_2 (new pcl::PointCloud<PFHSignature125>);
 
-	cout << "Puntos tras VG: " << cloud_filtered->size() << endl;
+    pcl::VoxelGrid<pcl::PointXYZRGB > vGrid;
+    vGrid.setInputCloud (cloud_1);
+    vGrid.setLeafSize (0.05f, 0.05f, 0.05f);
+    vGrid.filter (*cloud_filtered_1);
+    cout << "Puntos tras VG_1: " << cloud_filtered_1->size() << endl;
+    cloud_1 = cloud_filtered_1;
 
-	visu_pc = cloud_filtered;
+    vGrid.setInputCloud (cloud_2);
+    vGrid.filter (*cloud_filtered_2);
+    cout << "Puntos tras VG_2: " << cloud_filtered_2->size() << endl;
+    cloud_2 = cloud_filtered_2;  
+    
+    pcl::PointCloud<pcl::Normal>::Ptr normals_1 (new pcl::PointCloud<pcl::Normal>);
+    pcl::PointCloud<pcl::Normal>::Ptr normals_2 (new pcl::PointCloud<pcl::Normal>);
+    const float normal_radius = 0.03;
 
-  // Detectamos los keypoints 
-  detect_keypoints(visu_pc, 0.005f, 6, 4, 0.005f);
+    compute_surface_normals(cloud_filtered_1, normal_radius, normals_1);  
+    compute_surface_normals(cloud_filtered_2, normal_radius, normals_2); 
+
+    // Detectamos los keypoints 
+    //cout << "antes de entrar en la deteccion de los keypoints" << endl;
+    detect_keypoints(cloud_1, *pcKeyPoints_1, 0.005f, 6, 4, 0.005f);
+    detect_keypoints(cloud_2, *pcKeyPoints_2, 0.005f, 6, 4, 0.005f);
+
+    std::cout << "No of SIFT points in the keypoints_1 are " << pcKeyPoints_1->points.size () << std::endl;
+    std::cout << "No of SIFT points in the keypoints_2 are " << pcKeyPoints_2->points.size () << std::endl;
+    //cout << "paso la deteccion" << endl;
+    if(pcKeyPoints_1->size() > 10 && pcKeyPoints_2->size() > 10){
+      cout << "paso por el if" << endl;
+      // features
+      PFH(cloud_filtered_1, normals_1, pcKeyPoints_1, *cloudDescriptors_1, 0.05f);
+      PFH(cloud_filtered_2, normals_2, pcKeyPoints_2, *cloudDescriptors_2, 0.05f);
+
+      std::cout << "No of PFH points in the descriptors_1 are " << cloudDescriptors_1->points.size () << std::endl;
+      std::cout << "No of PFH points in the descriptors_2 are " << cloudDescriptors_2->points.size () << std::endl;
+    } 
+  }
 }
 
 int main(int argc, char** argv)
@@ -126,13 +233,13 @@ int main(int argc, char** argv)
   ros::NodeHandle nh;
   ros::Subscriber sub = nh.subscribe<pcl::PointCloud<pcl::PointXYZRGB> >("/camera/depth/points", 1, callback);
   // Descomentar para teleoperar
-  ros::Publisher cmd_vel_pub_ = nh.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/teleop", 1);
+  //ros::Publisher cmd_vel_pub_ = nh.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/teleop", 1);
   boost::thread t(simpleVis);
 
   while(ros::ok())
   {
     // Esto funciona pero habria que buscar la manera de hacerlo solo cuando queramos y no siempre
-  	driveKeyboard(cmd_vel_pub_);
+  	//driveKeyboard(cmd_vel_pub_);
 		ros::spinOnce();
   }
 
